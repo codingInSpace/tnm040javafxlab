6f6d150da4f5765159ee084a54078337e5feb864
Jonathan <jonathan.grangien@gmail.com>
2015-09-15 22:32:12 +0200

    Tillämpa press me-knapp med meddelande

--- src/application/Scene2Controller.java
+++ src/application/Scene2Controller.java
@@ -5,12 +5,16 @@ import javax.swing.JOptionPane;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
+//import javafx.scene.control.MenuItem;
+import javafx.scene.text.Text;
 
 public class Scene2Controller {
 
 //	@FXML
 //	protected MenuItem closeButton;
+
+	@FXML //fx:id = content_message
+	public Text footer_message;
 
 	public void closeApplication(ActionEvent event) {
 //        System.out.println("hej.");
@@ -19,6 +23,14 @@ public class Scene2Controller {
 
 	public void showAbout(ActionEvent event) {
 		JOptionPane.showMessageDialog(null, "hej det här är en bra app.");
+	}
+
+	public void niceMessage(ActionEvent event) {
+		try {
+			footer_message.setText("hej nu hände nånting kul");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
