f2b1884c3172e000af25f82d1428827404b437c1
Jonathan <jonathan.grangien@gmail.com>
2015-09-15 22:10:14 +0200

    Lägg till showAbout för help - about

--- src/application/Scene2Controller.java
+++ src/application/Scene2Controller.java
@@ -1,5 +1,7 @@
 package application;
 
+import javax.swing.JOptionPane;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,12 +9,16 @@ import javafx.scene.control.MenuItem;
 
 public class Scene2Controller {
 
-	@FXML
-	protected MenuItem closeButton;
+//	@FXML
+//	protected MenuItem closeButton;
 
 	public void closeApplication(ActionEvent event) {
 //        System.out.println("hej.");
         Platform.exit();
     }
+
+	public void showAbout(ActionEvent event) {
+		JOptionPane.showMessageDialog(null, "hej det här är en bra app.");
+	}
 
 }
