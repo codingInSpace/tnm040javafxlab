43d84b0f6cf6f5b9a1c945744ae0c0acfe3908bb
Jonathan <jonathan.grangien@gmail.com>
2015-09-16 22:56:19 +0200

    Fixa med kommentarer

--- src/application/Scene2Controller.java
+++ src/application/Scene2Controller.java
@@ -10,22 +10,18 @@ import javafx.scene.text.Text;
 
 public class Scene2Controller {
 
-//	@FXML
-//	protected MenuItem closeButton;
-
-	@FXML //fx:id = content_message
+	@FXML //fx:id = footer_message
 	public Text footer_message;
 
 	public void closeApplication(ActionEvent event) {
-//        System.out.println("hej.");
         Platform.exit();
     }
 
-	public void showAbout(ActionEvent event) {
+	public void showAbout() {
 		JOptionPane.showMessageDialog(null, "hej det här är en bra app.");
 	}
 
-	public void niceMessage(ActionEvent event) {
+	public void niceMessage() {
 		try {
 			footer_message.setText("hej nu hände nånting kul");
 		} catch(Exception e) {
